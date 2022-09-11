3a8accd3e837eccddb3e19bd1d4004ddef118668
mikhaellam <111900648+mikhaellam@users.noreply.github.com>
2022-09-12 13:25:50 +1400

    Delete SQLite.4.sql

--- SQLite.4.sql
+++
@@ -1,5 +0,0 @@
-create table Shippings(Shipping_ID int, Status int, Customer int);
-           insert into Shippings(Shipping_ID, Status, Customer) values (1, Pending, 2);
-           insert into Shippings(Shipping_ID, Status, Customer) values (2, Delivered, 3);
-           insert into Shippings(Shipping_ID, Status, Customer) values (3, Delivered, 1);
-           insert into Shippings(Shipping_ID, Status, Customer) values (4, Pending, 4);
